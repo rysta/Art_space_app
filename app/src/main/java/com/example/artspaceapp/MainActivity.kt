cc01ef607c26667ead2605a1e93405d5a4a12fdc
rysta <rysta@mail.ru>
2023-05-30 09:58:10 +0300

    вёрстка

--- app/src/main/java/com/example/artspaceapp/MainActivity.kt
+++ app/src/main/java/com/example/artspaceapp/MainActivity.kt
@@ -3,13 +3,31 @@ package com.example.artspaceapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.artspaceapp.ui.theme.ArtSpaceAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,15 +40,78 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    ArtScreen()
                 }
             }
         }
     }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun ArtScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize().padding(top = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Surface(
+            tonalElevation = 12.dp,
+            color = Color.White,
+            shadowElevation = 12.dp
+        ) {
+            Image(
+                painterResource(R.drawable.horse_woman),
+                contentDescription = "test",
+                modifier = Modifier.padding(20.dp)
+            )
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(
+            stringResource(R.string.horse_woman_title),
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Row(horizontalArrangement = Arrangement.Center) {
+            Text(
+                stringResource(R.string.horse_woman_author),
+                fontSize = 16.sp
+            )
+            Text(
+                buildAnnotatedString {
+                append(" ")
+                append("(")
+                append(stringResource(R.string.horse_woman_year))
+                append(")")
+                },
+                fontSize = 16.sp
+            )
+        }
+    }
+    Row(
+        modifier = Modifier.fillMaxWidth().padding(start = 12.dp, end = 12.dp, bottom = 12.dp),
+        verticalAlignment = Alignment.Bottom,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.weight(0.5f)
+        ) {
+            Text("Previous")
+        }
+        Spacer(modifier = Modifier.width(20.dp))
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.weight(0.5f)
+        ) {
+            Text("Next")
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     ArtSpaceAppTheme {
+        ArtScreen()
     }
 }
